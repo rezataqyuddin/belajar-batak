4be8c379b96aecf8108578924c8f58e2d565f258
Reza Taqyuddin <rezataqyuddin@gmail.com>
2023-06-21 12:17:12 +0700

    Batak Unicode

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -832,7 +832,7 @@
 </head>
 
 <body class="antialiased">
-ᯘᯮᯒᯖ᯲ ᯅᯖᯂ᯲
+    <h1 style="font-size:42px">ᯒᯖ᯲</h1> ᯅᯖᯂ᯲
     <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         @if (Route::has('login'))
         <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
